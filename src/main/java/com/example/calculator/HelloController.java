8be933e2b7ad39602bb25fd847b8230210a3730b
Maychii <106334255+TanyasiPills@users.noreply.github.com>
2024-12-13 15:27:23 +0100

    fix commit

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -8,18 +11,34 @@ import javafx.scene.control.TextField;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.swing.event.ChangeEvent;
 import java.awt.event.ActionEvent;
 
 public class HelloController {
 
     @FXML
-    private TextField first,second;
+    private TextField first;
+    @FXML
+    private TextField second;
 
     @FXML
     private Label bytext;
 
     @FXML
     private Button sup1, sup2, sup3, sup4, sup5;
+
+    public void initialize() {
+        second.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                second.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        first.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                first.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
 
     public void onButtonClick(javafx.event.ActionEvent actionEvent) {
         try {
@@ -36,9 +55,25 @@ public class HelloController {
                     bytext.setText(String.valueOf((num1 * num2)));
                     break;
                 case "/":
-                    bytext.setText(String.valueOf((num1 / num2)));
+                    if (num2 == 0) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Nem lehet nulla az osztó");
+                        alert.showAndWait();
+                        return;
+                    }
+                    bytext.setText(String.format("%.2f", num1 / num2));
                     break;
                 case "%":
+                    if (num2 == 0) {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("Error");
+                        alert.setHeaderText(null);
+                        alert.setContentText("Nem lehet nulla a moduló");
+                        alert.showAndWait();
+                        return;
+                    }
                     bytext.setText(String.valueOf((num1 % num2)));
                     break;
             }
